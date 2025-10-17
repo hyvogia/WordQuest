2f2ef91f79e511492cf96a824634ed76a55902e2
Hy Vo <94402902+hvo11152021@users.noreply.github.com>
2025-10-17 14:39:19 -0500

    Created login UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,27 @@
-import Hello from "./components/Hello";
+import { BrowserRouter, Route, Routes } from "react-router";
+//import Hello from "./components/Hello";
+
+function Login() {
+  return (
+    <div className="m-2 flex flex-col items-center justify-center min-h-screen">
+      <h1>Login</h1>
+      <form className="m-2 flex flex-col items-center justify-center">
+        <input className="p-2 m-2 border rounded" placeholder="username" />
+        <input className="p-2 m-2 border rounded" placeholder="password" />
+        <button className="m-2 border p-2 rounded">Join Game</button>
+      </form>
+    </div>
+  )
+}
+
 function Main() {
 
   return (
-    <Hello />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 

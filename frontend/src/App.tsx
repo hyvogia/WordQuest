4e511ac680a501f1033d3e2a71f49cd1e4e653a7
Hy Vo <94402902+hvo11152021@users.noreply.github.com>
2025-10-17 14:41:57 -0500

    Created register UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,12 +14,26 @@ function Login() {
   )
 }
 
+function SignUp() {
+  return (
+    <div className="m-2 flex flex-col items-center justify-center min-h-screen">
+      <h1>Register</h1>
+      <form className="m-2 flex flex-col items-center justify-center">
+        <input className="p-2 m-2 border rounded" placeholder="username" />
+        <input className="p-2 m-2 border rounded" placeholder="password" />
+        <button className="m-2 border p-2 rounded">Sign Up</button>
+      </form>
+    </div>
+  )
+}
+
 function Main() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   )

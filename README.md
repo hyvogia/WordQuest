0dc3c6d03f6a1741056555406c841b4ee35ba83a
Hy Vo <94402902+hyvogia@users.noreply.github.com>
2026-08-11 10:23:08 -0500

    Revert "Create README.md, fix minor issues"
    This reverts commit e73fa6896ecb9637bf8d61927349a8115235613f.

--- README.md
+++
@@ -1,7 +0,0 @@
-# Word Quest - Synonym Exercise 
-
-React + Node.js + MySQL
-
-## Visuals
-
-<img src="wq1.png">
--- backend/dist/models/userModel.js
+++ backend/dist/models/userModel.js
@@ -4,7 +4,7 @@ export const users = [
     { username: "Carol", password: "carol123" }
 ];
 export const scores = [
-// { username: "Andrew", score: 0 }
+// example: { username: "Andrew", score: 0 }
 ];
 export const questions = [
     {
